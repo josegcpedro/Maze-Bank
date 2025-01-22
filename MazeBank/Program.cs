7b3db768d655a990600a3db7c4bba8be9187fcf5
Jose <pedrojcunha7@hotmail.com>
2025-01-22 14:29:59 +0100

    feat: added menu with options

--- MazeBank/Program.cs
+++ MazeBank/Program.cs
@@ -27,22 +27,37 @@ class Program
         if (account == null)
         {
             Console.WriteLine("Compte pas trouvé. Système de securité activé!");
-        } else {
+        }
+        else
+        {
             Menu(account);
         }
-
-        static void Menu(Account account){
-            Console.WriteLine($"Bienvenue {account.Name}");
-        }
-
-
-
-
-
-
-
-
     }
+    static void Menu(Account account)
+    {
+        Console.WriteLine($"Bienvenue {account.Name} que voulez vous faire?");
+        Console.WriteLine("1.Retirer de l'argent");
+        Console.WriteLine("2.Ajouter de l'argent");
+        Console.WriteLine("3.Voir solde bancaire");
+        Console.WriteLine("4.Quitter");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                //Withdraw();
+                break;
+            case "2":
+                //Add();
+                break;
+            case "3":
+                //VerifyMoneyAccount();
+                break;
+            case "4":
+                return;
+        }
+    }
+
 }
 
 public class Account
